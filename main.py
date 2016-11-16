462fa6749e0ee6fea41ed8d75c4e8546044b09e3
Flynn Tesoriero <flynntes@users.noreply.github.com>
2016-11-16 20:09:43 +1100

    Completed player vs player. About to start player vs computer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #TIC TAC TOE BY FLYNN TESORIERO
 
+#For delays
+import time
+
 #Default Positions
 positions = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
@@ -30,16 +33,27 @@ global i
 winner = False
 i = 1
 
+print("===================================\n"+colour.blue+colour.bold+" Welcome to Tic Tac Toe in Python!"+colour.end+"\n===================================\n")
+
+#Colour the X & O's on the board in realtime without changing list
+def poscolour(pos, number):
+    if(pos[number] == 'x'):
+        return colour.green + str(pos[number]) + colour.end
+    elif (pos[number] == 'o'):
+        return colour.purple + str(pos[number]) + colour.end
+    else:
+        return pos[number]
+
 #Function to print board
 #1 (a) is in the bottom left corner, like a num pad.
 #9 (i) in in the top right corner.
 def board(pos):
-    print("\n┌───┬───┬───┐")
-    print("│ "+pos[6]+" │ "+pos[7]+" │ "+pos[8]+" │")
+    print(colour.end+"\n┌───┬───┬───┐")
+    print("│ "+poscolour(positions,6)+" │ "+poscolour(positions,7)+" │ "+poscolour(positions,8)+" │")
     print("├───┼───┼───┤")
-    print("│ "+pos[3]+" │ "+pos[4]+" │ "+pos[5]+" │")
+    print("│ "+poscolour(positions,3)+" │ "+poscolour(positions,4)+" │ "+poscolour(positions,5)+" │")
     print("├───┼───┼───┤")
-    print("│ "+pos[0]+" │ "+pos[1]+" │ "+pos[2]+" │")
+    print("│ "+poscolour(positions,0)+" │ "+poscolour(positions,1)+" │ "+poscolour(positions,2)+" │")
     print("└───┴───┴───┘")
 
 #board(positions)
@@ -50,7 +64,6 @@ def inputcheck(low, high, input):
     try:
         int(input)
     except ValueError:
-        print("Please enter a valid number")
         return False
     else:
         parsedinput = int(input)
@@ -89,6 +102,29 @@ def check_win(pos, xo):
         elif (pos[2] == xo and pos[4] == xo and pos[6] == xo):
             return True
 
+def win(xo):
+    if (xo == "x"):
+        print(colour.green + "\n __  __   __        __  ___   _   _   ____  ")
+        time.sleep(0.5)
+        print(" \ \/ /   \ \      / / |_ _| | \ | | / ___| ")
+        time.sleep(0.5)
+        print("  \  /     \ \ /\ / /   | |  |  \| | \___ \ ")
+        time.sleep(0.5)
+        print("  /  \      \ V  V /    | |  | |\  |  ___) |")
+        time.sleep(0.5)
+        print(" /_/\_\      \_/\_/    |___| |_| \_| |____/ "+colour.end)
+        time.sleep(0.5)
+    elif (xo == "o"):
+        print(colour.purple + "\n   ___     __        __  ___   _   _   ____  ")
+        time.sleep(0.5)
+        print("  / _ \    \ \      / / |_ _| | \ | | / ___| ")
+        time.sleep(0.5)
+        print(" | | | |    \ \ /\ / /   | |  |  \| | \___ \ ")
+        time.sleep(0.5)
+        print(" | |_| |     \ V  V /    | |  | |\  |  ___) |")
+        time.sleep(0.5)
+        print("  \___/       \_/\_/    |___| |_| \_| |____/ "+colour.end)
+        time.sleep(0.5)
 
 #For checking the space
 #Takes in a position an position number
@@ -96,7 +132,7 @@ def check_space(pos, num):
     #print(pos[num])
     if(pos[num] != " "):
         #print(pos[num])
-        print("That spot is taken!")
+        print("\nThat spot is taken! Choose a different spot")
         return False
     else:
         return True
@@ -105,7 +141,6 @@ def check_space(pos, num):
 #Takes in either an 'x' or an 'o'
 def choose(xo):
     global winner
-    print("-------")
     print("Choose a space (1-9)")
     space = int(input("Space: "))
     space = space - 1
@@ -115,38 +150,33 @@ def choose(xo):
         board(positions)
 
         if check_win(positions, xo):
-            print("Winner!!!")
+            win(xo)
             winner = True
         return True
     else:
         return False
 
-#winner variable may be an issue (not global or some shit)
+
 def player_vs_player():
     print(colour.bold+"\n-------------\n Begin Game!\n-------------"+colour.end)
     board(positions)
     global i
     while(winner != True):
         if (i % 2 == 0):
-            print("Player O")
+            print(colour.purple+"It's player O's turn!")
             if choose('o'):
                 i = i + 1
-            else:
-                print("choose again")
+            #else:
+            #    print("choose again")
         else:
-            print(colour.yellow+"Player X"+colour.end)
+            print(colour.green+"It's player X's turn!")
             if choose('x'):
                 i = i + 1
-            else:
-                print("choose again")
-
-
-    #All good things must come to an...
-    print("End")
+            #else:
+            #    print("choose again")
 
 
 def player_vs_computer():
-    #You like it hard or soft ;)
     print("\nSelect Difficultly")
     print("1: Simple")
     print("2: Hard")
@@ -168,14 +198,17 @@ def play():
     print("2: Player vs Computer")
 
     game_selection = input("Selection: ")
+
     game_selection = int(game_selection)
-    if (game_selection == 1):
-        player_vs_player()
-    elif (game_selection == 2):
-        player_vs_computer()
 
-
-
+    if (inputcheck(1, 2, game_selection) != False):
+        game_selection = inputcheck(1, 2, game_selection)
+        if (game_selection == 1):
+            player_vs_player()
+        elif (game_selection == 2):
+            player_vs_computer()
+    else:
+        print(colour.red + "That's not a choice...\n" + colour.end)
 
 def help():
     print(colour.bold+"\n------\n Help\n------"+colour.end)
@@ -195,7 +228,6 @@ def help():
 
 #Menu
 def welcome():
-    print("===================================\n"+colour.green+colour.bold+" Welcome to Tic Tac Toe in Python!"+colour.end+"\n===================================\n")
     print(colour.bold+"Select one of the below options:"+colour.end)
     print("1. Play")
     print("2. Help")
@@ -213,18 +245,20 @@ def menu():
         elif (menu_selection == 2):
             help()
         else:
-            print("Bye")
+            print(colour.green+"Bye")
             quit()
     else:
-        print(colour.red+"Please choose again\n"+colour.end)
+        print(colour.red+"That's not a choice...\n"+colour.end)
 
 
 #Let's get this show on the road!
-#coltest()
 welcome()
 
 while True:
     menu()
 
+#Unit Tests:
+#coltest()
 #test = input("test: ")
 #inputcheck(1,10,test)
+
